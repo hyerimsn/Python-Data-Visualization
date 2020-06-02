3b89efb931a75b6e4a233b5e3797d0e1d570a01d
hyerimsn <etcheef@likelion.org>
2020-06-03 00:35:55 +0900

    이전고시 삭제 및 circlemarker 표시

--- reconmap/recon.py
+++ reconmap/recon.py
@@ -37,27 +37,35 @@ for item in data:
     step = item.find('step').get_text() #진행수준
     step_list.append(step)
 
+
 df = pd.DataFrame({'Address':addr_list, 'Step':step_list})
 
+idx = df[df['Step'] == '이전고시'].index
+df2 = df.drop(idx)
+df2 = df2.reset_index(drop=True)
 # for i in range(len(total_list)):
 #     info = {'Address' : total_list[i][0], 'Step' : total_list[i][1]}
+
+print(df2)
 
 lat = []
 lng = []
 
-for i in range(181):
-    gmaps_output = gmaps.geocode(df['Address'][i])
+for i in range(0,167):
+    gmaps_output = gmaps.geocode(df2['Address'][i])
     location_output = gmaps_output[0].get('geometry')
     lat.append(location_output['location']['lat'])
     lng.append(location_output['location']['lng'])
 
-df['lat'] = lat
-df['lng'] = lng
+df2['lat'] = lat
+df2['lng'] = lng
 
-mapping = folium.Map(location = [df['lat'].mean(), df['lng'].mean()], zoom_start=11)
+mapping = folium.Map(location = [df2['lat'].mean(), df2['lng'].mean()], zoom_start=11)
 
-for i in range(180):
-    folium.Marker([df['lat'][i], df['lng'][i]], popup=df['Address'][i]).add_to(mapping)
+for i in range(0,167):
+    folium.CircleMarker(radius = 2, color = 'red', fill=True, location = [df2['lat'][i], df2['lng'][i]], popup=df['Address'][i]).add_to(mapping)
+
+# print(df)
 
 filepath=r"C:\Users\USER\Desktop\SWEN\reconmap\recon.html"
 mapping.save(filepath)
